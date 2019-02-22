559051f175d5e29cb7d617ce8fdd28ef43dc0cca
Isaac Chaverri <Isaaccorruma05@hotmail.com>
2019-02-21 23:46:51 -0600

    Crea grafo y tabla de heurística desde txt
    Self explanatory, ciertas mejoras de rendimiento

--- CrearGrafo/LlenarGrafo.cs
+++ CrearGrafo/LlenarGrafo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CrearGrafo
@@ -8,42 +9,16 @@ namespace CrearGrafo
     {
         public List<Nodo> Grafo { set; get; }
         public List<Arista> AristasGlobales { set; get; }
+        public Dictionary<string, int> Heuristica { set; get; }
         public void inicio()
         {
             Grafo = new List<Nodo>();
             AristasGlobales = new List<Arista>();
-            creaGrafo();
+            Heuristica = new Dictionary<string, int>();
+            ReadFile();          
             ImprimirGrafo(Grafo);
         }
-        public void creaGrafo()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Grafo.Add(new Nodo(Convert.ToString(i + 1)));
-            }
-            creaAristas();
-            foreach (Arista arista in AristasGlobales)
-            {
-                foreach (Nodo nodo in Grafo)
-                {
-                    if (arista.partida.Equals(nodo))
-                    {
-                        nodo.AgregaArista(arista);
-                    }
-                }
-            }
 
-        }
-
-        public void creaAristas()
-        {
-            AristasGlobales.Add(new Arista(7, BuscaNodo("2"), BuscaNodo("4")));
-            AristasGlobales.Add(new Arista(15, BuscaNodo("1"), BuscaNodo("2")));
-            AristasGlobales.Add(new Arista(6, BuscaNodo("3"), BuscaNodo("2")));
-            AristasGlobales.Add(new Arista(4, BuscaNodo("5"), BuscaNodo("4")));
-            AristasGlobales.Add(new Arista(20, BuscaNodo("4"), BuscaNodo("3")));
-            AristasGlobales.Add(new Arista(10, BuscaNodo("1"), BuscaNodo("5")));
-        }
 
         public Nodo BuscaNodo(String nombre)
         {
@@ -58,6 +33,8 @@ namespace CrearGrafo
             return null;
 
         }
+
+
         public void ImprimirGrafo(List<Nodo> Grafo)
         {
             foreach (Nodo nodo in Grafo)
@@ -70,7 +47,40 @@ namespace CrearGrafo
                     Console.WriteLine("Destino: " + arista.destino.Nombre + "\n");
                 }
             }
+            foreach (KeyValuePair<string, int> Item in Heuristica)
+                Console.WriteLine(Item.Key + ":" + Heuristica[Item.Key]);
             char i = Console.ReadKey().KeyChar;
+        }
+
+        public void ReadFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Isaac\Desktop\Cosas-De-La-U\Semestre XIII\Inteligencia Artificial\Tareas\Tareas 3 y 4\IA\Prueba1.txt");
+            foreach (string line in lines)
+            {
+                string[] linea = line.Split(new Char[] { ',',':', ' ' });
+                switch (linea.Length)
+                {
+                    case 1:
+                        Grafo.Add(new Nodo(linea[0]));
+                        break;
+                    case 2:
+                        Heuristica.Add(linea[0], Convert.ToInt32(linea[1]));
+                        break;
+                    case 3:
+                        AristasGlobales.Add(new Arista(Convert.ToInt32(linea[0]), BuscaNodo(linea[1]), BuscaNodo(linea[2])));
+                        break;
+                }
+            }
+            foreach (Arista arista in AristasGlobales)
+            {
+                foreach (Nodo nodo in Grafo)
+                {
+                    if (arista.partida.Equals(nodo))
+                    {
+                        nodo.AgregaArista(arista);
+                    }
+                }
+            }
         }
     }
 }
