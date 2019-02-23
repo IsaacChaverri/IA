f126421eba214000a2b7df57f5a1f1994a6201a2
MaoEl26 <m.c.v.2695@gmail.com>
2019-02-23 15:27:05 -0600

    Algoritmo A*

--- CrearGrafo/LlenarGrafo.cs
+++ CrearGrafo/LlenarGrafo.cs
@@ -10,11 +10,13 @@ namespace CrearGrafo
         public List<Nodo> Grafo { set; get; }
         public List<Arista> AristasGlobales { set; get; }
         public Dictionary<string, int> Heuristica { set; get; }
+        public List<ObjetoCamino> listaRecorrido;
         public void inicio()
         {
             Grafo = new List<Nodo>();
             AristasGlobales = new List<Arista>();
             Heuristica = new Dictionary<string, int>();
+            listaRecorrido = new List<ObjetoCamino>();
             ReadFile();          
             ImprimirGrafo(Grafo);
         }
@@ -49,12 +51,15 @@ namespace CrearGrafo
             }
             foreach (KeyValuePair<string, int> Item in Heuristica)
                 Console.WriteLine(Item.Key + ":" + Heuristica[Item.Key]);
+
+            MeteVecinos(BuscaNodo("Cartago"), BuscaNodo("TEC")); /* Metodo prueba*/
+
             char i = Console.ReadKey().KeyChar;
         }
 
         public void ReadFile()
         {
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Isaac\Desktop\Cosas-De-La-U\Semestre XIII\Inteligencia Artificial\Tareas\Tareas 3 y 4\IA\Prueba1.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"E:\IA\IsaacChaverri\IA\Prueba1.txt");
             foreach (string line in lines)
             {
                 string[] linea = line.Split(new Char[] { ',',':', ' ' });
@@ -82,5 +87,80 @@ namespace CrearGrafo
                 }
             }
         }
+
+        public void MeteVecinos(Nodo nodoInicio, Nodo nodoFinal)
+        {
+            ObjetoCamino temp = null;
+            ObjetoCamino objetoPivote = null;
+            if (listaRecorrido.Count == 0)
+            {
+                Console.WriteLine(listaRecorrido.Count);
+                foreach (Arista arista in nodoInicio.Aristas)
+                {
+                    temp = new ObjetoCamino();
+                    temp.nombre = arista.destino.Nombre;
+                    temp.value = arista.peso + Heuristica[arista.destino.Nombre];
+                    temp.ruta.Add(arista.partida.Nombre);
+                    listaRecorrido.Add(temp);
+                }
+            }
+            else
+            {
+                
+                foreach (ObjetoCamino obj in listaRecorrido)
+                {
+                    if (obj.nombre.Equals(nodoInicio.Nombre))
+                    {
+                        objetoPivote = obj;
+                    }
+                }
+                listaRecorrido.Remove(objetoPivote);
+                foreach (Arista arista in nodoInicio.Aristas)
+                {
+                    temp = new ObjetoCamino();
+                    temp.nombre = arista.destino.Nombre;
+                    temp.value = arista.peso + Heuristica[arista.destino.Nombre] + objetoPivote.value;
+                    temp.ruta = objetoPivote.ruta;
+                    temp.ruta.Add(arista.partida.Nombre);
+                    listaRecorrido.Add(temp);
+                }
+            }
+            int pivote = int.MaxValue;
+            foreach (ObjetoCamino obj in listaRecorrido)
+            {
+                if (objetoPivote != null)
+                {
+                    if (obj.value <= pivote && objetoPivote.nombre.Equals(obj.ruta[obj.ruta.Count - 1]))
+                    {
+                        pivote = obj.value;
+                        temp = obj;
+                    }
+                }
+                else
+                {
+                    if (obj.value <= pivote)
+                    {
+                        pivote = obj.value;
+                        temp = obj;
+                    }
+                }
+            }
+
+            Console.WriteLine("Temporal: " + temp.nombre + " Final: " + nodoFinal.Nombre + "\n");
+            if (!temp.nombre.Equals(nodoFinal.Nombre))
+            {
+                Nodo tempo = BuscaNodo(temp.nombre);
+                
+                MeteVecinos(tempo, nodoFinal);
+                Console.WriteLine(tempo.Nombre);
+            }
+        }
+    }
+
+    class ObjetoCamino
+    {
+        public String nombre { set; get; }
+        public int value { set; get; }
+        public List<String> ruta = new List<String>();
     }
 }
